0bc1bd75ac710f1aa09add1c2f370d930287bc56
vs3kulic <sekulicvajo@gmail.com>
2025-03-29 10:29:57 +0100

    updated collatz-conjecture

--- collatz-conjecture/collatz.py
+++ collatz-conjecture/collatz.py
@@ -2,11 +2,12 @@ def steps(number):
     if not isinstance(number, int) or number <= 0:
         raise ValueError("Only positive integers are allowed")
     
-    counter = 0
-    while number != 1: 
-        if number % 2 == 0: 
-            number = number // 2
+    def recursive_steps(num, counter=0):
+        if num == 1:
+            return counter
+        if num % 2 == 0:
+            return recursive_steps(num // 2, counter + 1)
         else:
-            number = 3 * number + 1
-        counter += 1
-    return counter
+            return recursive_steps(3 * num + 1, counter + 1)
+    
+    return recursive_steps(number)
