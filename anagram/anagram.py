f1b26c9dbb05d6199f81d5e04fd006ac17de193c
vs3kulic <sekulicvajo@gmail.com>
2025-03-30 12:58:27 +0200

    updated anagram

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,6 +1,7 @@
 def find_anagrams(word, candidates):
-    sorted_word = sorted(list(word))
-    sorted_candidates = [sorted(list(candidate)) for candidate in candidates]
+    not_anagram = [candidates.remove(candidat) for candidat in candidates if candidat.lower() == word.lower()]
+    sorted_word = sorted(list(word.lower()))
+    sorted_candidates = [sorted(list(candidate.lower())) for candidate in candidates]
     anagrams = [candidate for candidate, sorted_candidate in zip(candidates, sorted_candidates) if sorted_candidate == sorted_word]
     
     return anagrams
