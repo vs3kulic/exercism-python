c0dc440c2dd437d061c31787466b14101f633c84
vs3kulic <sekulicvajo@gmail.com>
2025-06-18 17:33:23 +0200

    fix(loops): formatted strings

--- list-ops/list_ops.py
+++ list-ops/list_ops.py
@@ -4,8 +4,9 @@ from typing import Callable, Any
 def length(lst: list) -> int:
     """Return the length of the list."""
     _validate_list(lst)
+    list_length = len(lst)
 
-    return len(lst)
+    return list_length
 
 
 def append(list1: list, list2: list) -> list:
@@ -13,7 +14,7 @@ def append(list1: list, list2: list) -> list:
     # Validate inputs
     _validate_list(list1)
     _validate_list(list2)
-
+    
     # Copy list to avoid modifying in place, then extend the copy
     extended_list = list1.copy()
     extended_list.extend(list2)
@@ -25,21 +26,18 @@ def concat(lists: list[list]) -> list:
     """Concatenate a list of lists into a single list."""
     # Validate input
     _validate_list(lists)
-
-    # Initialise an empty list, then iterate through each list to extend the concatenated list
     concatenated_list = []
 
-    for lst in lists: 
+    for lst in lists:
         concatenated_list.extend(lst)
-    
+
     return concatenated_list
 
 
 def map(function: Callable, lst: list) -> list:
     """Apply a function to each element in the list and return a new list."""
     _validate_list(lst)
-
-    mapped_list = [function(item) for item in lst]
+    mapped_list = [function(item) for item in lst] # Apply function to each item
     
     return mapped_list
 
@@ -47,7 +45,6 @@ def map(function: Callable, lst: list) -> list:
 def filter(function: Callable, lst: list) -> list:
     """Filter elements in a list based on a function."""
     _validate_list(lst)
-
     filtered_list = [item for item in lst if function(item)]
     
     return filtered_list
@@ -64,7 +61,6 @@ def foldl(function: Callable, lst: list, initial: Any) -> Any:
     Example: foldl(lambda acc, el: el * acc, [1, 2, 3], 1) returns 6.
     """
     _validate_list(lst)
-
     accumulator = initial # Initialise accumulator with the initial value
     
     for item in lst:
@@ -76,7 +72,6 @@ def foldl(function: Callable, lst: list, initial: Any) -> Any:
 def foldr(function: Callable, lst: list, initial: Any) -> Any:
     """Fold a list from the right using a function and an initial value."""
     _validate_list(lst)
-
     accumulator = initial
 
     for item in reversed(lst):
@@ -88,7 +83,6 @@ def foldr(function: Callable, lst: list, initial: Any) -> Any:
 def reverse(lst: list) -> list:
     """Reverse the input list and return it."""
     _validate_list(lst)
-
     reversed_list = lst[::-1]  # Slicing to reverse the list
 
     return reversed_list
