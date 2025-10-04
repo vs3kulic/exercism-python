bda60d0b302699979260f01755b21adfb389f2ac
vs3kulic <sekulicvajo@gmail.com>
2025-10-04 11:37:05 +0200

    chore(classes): added kindergarten-garden

---
+++ kindergarten-garden/kindergarten_garden.py
@@ -0,0 +1,22 @@
+"""This module defines a Garden class that represents a kindergarten garden."""
+
+STUDENT_NAMES = [
+    "Alice", "Bob", "Charlie", "David", "Eve", "Fred",
+    "Ginny", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry"
+]
+
+DIAGRAM_ENCODING = {
+    "R": "Radishes",
+    "C": "Clover",
+    "G": "Grass",
+    "V": "Violets"
+}
+
+class Garden:
+    """A class representing a kindergarten garden with students and their plants."""
+    def __init__(self, diagram, students):
+        pass
+
+    def plants(self, student) -> list[str]:
+        """Return the list of plants for the given student."""
+        pass
