6919eb397f0d188c9f6df59c3a34e26b240ab694
vs3kulic <sekulicvajo@gmail.com>
2025-10-05 15:48:08 +0200

    chore(classes): added Matrix problem

--- ghost-gobble-arcade-game/arcade_game.py
+++ ghost-gobble-arcade-game/arcade_game.py
@@ -8,7 +8,7 @@ def eat_ghost(power_pellet_active: bool, touching_ghost: bool) -> bool:
     :param touching_ghost: bool - is the player touching a ghost?
     :return: bool - can a ghost be eaten?
     """
-    return True if power_pellet_active and touching_ghost else False
+    return power_pellet_active and touching_ghost
 
 def score(touching_power_pellet: bool, touching_dot: bool) -> bool:
     """Verify that Pac-Man has scored when a power pellet or dot has been eaten.
@@ -17,7 +17,7 @@ def score(touching_power_pellet: bool, touching_dot: bool) -> bool:
     :param touching_dot: bool - is the player touching a dot?
     :return: bool - has the player scored or not?
     """
-    return True if touching_power_pellet or touching_dot else False
+    return touching_power_pellet or touching_dot
 
 def lose(power_pellet_active: bool, touching_ghost: bool) -> bool:
     """Trigger the game loop to end (GAME OVER) when Pac-Man touches a ghost without his power pellet.
@@ -26,7 +26,7 @@ def lose(power_pellet_active: bool, touching_ghost: bool) -> bool:
     :param touching_ghost: bool - is the player touching a ghost?
     :return: bool - has the player lost the game?
     """
-    return True if touching_ghost and not power_pellet_active else False
+    return touching_ghost and not power_pellet_active
 
 def win(has_eaten_all_dots: bool, power_pellet_active: bool, touching_ghost: bool) -> bool:
     """Trigger the victory event when all dots have been eaten.
@@ -36,4 +36,4 @@ def win(has_eaten_all_dots: bool, power_pellet_active: bool, touching_ghost: boo
     :param touching_ghost: bool - is the player touching a ghost?
     :return: bool - has the player won the game?
     """
-    return True if has_eaten_all_dots and not lose(power_pellet_active, touching_ghost) else False
+    return has_eaten_all_dots and not lose(power_pellet_active, touching_ghost)
