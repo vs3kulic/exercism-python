d20d1f046ba84f05e828329ff5a2032b7ec061fe
vs3kulic <sekulicvajo@gmail.com>
2025-11-14 11:48:57 +0100

    feat(functions): added solution to gigaseconds (datetime)

---
+++ gigasecond/gigasecond-katas/gigasecond_prepass.py
@@ -0,0 +1,19 @@
+"""This module contain a pre-pass implementation for the gigasecond exercise."""
+
+# --------------------------
+# INPUTS
+# --------------------------
+# We need to import the datetime module (from the standard library datetime package)
+# We will get a datetime object
+#   Example: datetime(2015, 1, 24, 23, 59, 59) represents January 24, 2015 at 23:59:59
+
+# --------------------------
+# PROCESSING
+# --------------------------
+# We convert the gigasecond (1,000,000,000 seconds) to a timedelta object
+# We add the timedelta to the input datetime object using the + operator
+
+# --------------------------
+# OUTPUTS
+# --------------------------
+# The output is a new datetime object representing the original datetime 1 gigasecond later
--- gigasecond/gigasecond.py
+++ gigasecond/gigasecond.py
@@ -1,2 +1,12 @@
-def add(moment):
-    pass
+from datetime import datetime, timedelta
+
+def add(moment: datetime) -> datetime:
+    """Add a gigasecond (1,000,000,000 seconds) to the given datetime.
+    
+    :param moment: The initial datetime
+    :type moment: datetime
+    :returns: A new datetime representing the moment 1 gigasecond later
+    :rtype: datetime
+    """
+    gigasecond = timedelta(seconds=1_000_000_000)
+    return moment + gigasecond
