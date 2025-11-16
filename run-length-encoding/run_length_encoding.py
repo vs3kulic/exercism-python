a4b48bfb06cd409116e5e9ef4d733d0992860f23
vs3kulic <sekulicvajo@gmail.com>
2025-11-16 16:34:30 +0100

    feat(functions): added working solution to rle

--- run-length-encoding/run_length_encoding.py
+++ run-length-encoding/run_length_encoding.py
@@ -1,6 +1,6 @@
 """This module contains functions for run-length encoding and decoding of strings."""
 
-def decode(code: str) -> str:
+def decode(encoded_string: str) -> str:
     """Decode a run-length encoded string.
     
     :param code: The run-length encoded string ("2a3b4c")
@@ -12,22 +12,27 @@ def decode(code: str) -> str:
     return run
 
 
-def encode(run: str) -> str:
-    """Encode a string using run-length encoding.
-    
-    param run: The string to encode ("aabbbcccc")
-    :type run: str
-    :returns: The run-length encoded string
-    :rtype: str
-    """
+def encode(decoded_string: str) -> str:
+    """Encode a string using run-length encoding."""
+    if not decoded_string:
+        return ""
+
     encoded = []
-    for i, char in enumerate(run):
-        count = 0
-        while i+1 < len(run):
-            if run[i] == run[i+1]:
-                count += 1
-                i += 1
-        encoded.append(f"{count}{char}")
+    i = 0
+    while i < len(decoded_string):
+        char = decoded_string[i]
+        count = 1
+        while (
+            i + 1 < len(decoded_string) and
+            decoded_string[i] == decoded_string[i+1]
+        ):
+            count += 1
+            i += 1
+        if count == 1:
+            encoded.append(char)
+        else:
+            encoded.append(f"{count}{char}")
+        i += 1
     return "".join(encoded)
 
 
